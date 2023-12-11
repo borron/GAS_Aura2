e5b3b9131c145ce77c1e1f2250bca4aad0b78244
Ronnie Bornerot <ronnie.bornerot@gmail.com>
2023-12-11 03:51:25 +0100

    Player and Enemy Characters

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,17 @@
+// Copyright 2024 Brisingamen Spelstudio
+
+
+#include "Character/AuraCharacterBase.h"
+
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
---
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -0,0 +1,8 @@
+// Copyright 2024 Brisingamen Spelstudio
+
+
+#include "Character/AuraEnemy.h"
+
+
+
+
